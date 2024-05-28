3fa474343a9ab63f607337aa4696d254279ad200
weitsung50110 <a1233z51ab10@gmail.com>
2024-05-28 10:39:53 +0800

    adding langchain_rag_doc.py comments and modify doc

--- langchain_rag_doc.py
+++ langchain_rag_doc.py
@@ -9,28 +9,38 @@ from langchain_core.documents import Document
 from langchain.text_splitter import CharacterTextSplitter
 
 
+# 初始化Ollama模型
 llm = Ollama(model='llama3')
 
+# 建立文件列表，每個文件包含一段文字內容
 docs = [
-    Document(page_content='變身水（Polyjuice Potion）可變成其他人的樣貌。不可拿來變身成動物，也對動物產生不了效果（包括半人半動物的生物），誤用動物毛髮的話，則會變成動物的容貌。'),
-    Document(page_content='吐真劑（Veritaserum）出自《火盃的考驗》，特徵為像水一樣清澈無味，使用者只要加入三滴，就能強迫飲用者說出真相。它是現存最強大的吐實魔藥，在《哈利波特》的虛構世界觀中受英國魔法部嚴格控管。J·K·羅琳表示，吐真劑最適合用在毫無戒心、易受傷害、缺乏自保技能的人身上，有些巫師能使用鎖心術等方式保護自己免受吐真劑影響。'),
-    Document(page_content='福來福喜（Felix Felicix）出自《混血王子》，是一種稀有而且難以調製的金色魔藥，能夠給予飲用者好運。魔藥的效果消失之前，飲用者的所有努力都會成功。假如飲用過量，會導致頭暈、魯莽和危險的過度自信，甚至成為劇毒。'),
+    Document(page_content='曼德珍珠奶茶草：這種植物具有強大的魔法屬性，常用於恢復被石化的受害者。'),
+    Document(page_content='山羊可愛蓮花石 ：是一種從山羊胃中取出的石頭，可以解百毒。在緊急情況下，它被認為是最有效的解毒劑。'),
+    Document(page_content='日本小可愛佐籐鱗片：這些鱗片具有強大的治愈能力，常用於製作治療藥水，特別是用於治療深層傷口。'),
 ]
 
-text_splitter = CharacterTextSplitter(chunk_size=20, chunk_overlap=5) #通過設置塊重疊部分，我們可以確保每個分割後的文本塊仍然包含足夠的上下文信息
-documents = text_splitter.split_documents(docs)
+# 設定文本分割器，chunk_size是分割的大小，chunk_overlap是重疊的部分
+text_splitter = CharacterTextSplitter(chunk_size=20, chunk_overlap=5)
+documents = text_splitter.split_documents(docs)  # 將文件分割成更小的部分
 
+# 初始化嵌入模型
 embeddings = OllamaEmbeddings()
 
+# 使用FAISS建立向量資料庫
 vectordb = FAISS.from_documents(docs, embeddings)
+# 將向量資料庫設為檢索器
 retriever = vectordb.as_retriever()
 
+# 設定提示模板，將系統和使用者的提示組合
 prompt = ChatPromptTemplate.from_messages([
     ('system', 'Answer the user\'s questions in Chinese, based on the context provided below:\n\n{context}'),
     ('user', 'Question: {input}'),
 ])
+
+# 創建文件鏈，將llm和提示模板結合
 document_chain = create_stuff_documents_chain(llm, prompt)
 
+# 創建檢索鏈，將檢索器和文件鏈結合
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
 context = []
