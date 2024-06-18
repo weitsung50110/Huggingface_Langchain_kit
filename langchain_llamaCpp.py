67d0c400cde6914b4306f674c2df55bd08f26e0d
weitsung50110 <a1233z51ab10@gmail.com>
2024-06-18 12:10:10 +0800

    modify langchain_llamaCpp.py

--- langchain_llamaCpp.py
+++ langchain_llamaCpp.py
@@ -6,12 +6,9 @@ from langchain_core.prompts import PromptTemplate
 # <s> [INST] <<SYS>>你是一個專業助理，
 # 請依照客戶的問題用專業的口吻回答<</SYS>>至少20字以上。[INST]
 # """
-template = """
-你是一個專業助理，你的名字是大蓮花，請依照客戶的問題用專業的口吻回答至少20字以上。
+template = """Question: {question}
 
-這是客戶的問題: {input}
-"""
-
+Answer: 讓我們解決這個問題，以確保我們得到正確的答案"""
 
 prompt = PromptTemplate.from_template(template)
 
@@ -22,9 +19,9 @@ callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 # Make sure the model path is correct for your system!
 llm = LlamaCpp(
     model_path="hugging_convert/Llama3-8B-Chinese-Chat-f16-v2_1.gguf",
-    temperature=0.2,
-    max_tokens=2000,
-    top_p=1,
+    # temperature=0.2,
+    # max_tokens=2000,
+    # top_p=1,
     callback_manager=callback_manager,
     verbose=True,  # Verbose is required to pass to the callback manager
 )
@@ -34,13 +31,19 @@ llm_chain = prompt | llm
 # question = "為什麼人會出生？"
 # llm_chain.invoke({"question": question})
 
-# question = """
-# Question: 你是誰？你的名字是什麼？
-# """
-# llm.invoke(question)
+input_text = input('>>> ')
+while input_text.lower() != 'bye':
+    response = llm_chain.invoke({
+        'question': input_text
+    })
+    input_text = input('>>> ')
 
 # {'title': 'PromptInput', 'type': 'object', 'properties':
 # question = {'question': '為什麼人會出生？', 'type': 'string'}
-llm_chain.invoke({
-        'input': input('>>> ')
-    })
+# input_text = input('>>> ')
+# while input_text.lower() != 'bye':
+#     response = llm_chain.invoke({
+#         'input': input_text
+#     })
+
+# https://python.langchain.com/v0.2/docs/integrations/llms/llamacpp/
