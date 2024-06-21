85f5b4f2b95af8ac9cac5244bb8be4258e963f41
weitsung50110 <a1233z51ab10@gmail.com>
2024-06-21 15:24:29 +0800

    add llamaCpp_sys_lover.py

--- langchain_llamaCpp_sys_lover.py
+++ langchain_llamaCpp_sys_lover.py
@@ -4,11 +4,13 @@ from langchain_core.prompts import PromptTemplate
 
 # template = """
 # <s> [INST] <<SYS>>你是一個專業助理，
-# 請依照客戶的問題用專業的口吻回答<</SYS>>至少20字以上。[INST]
+# 請依照客戶的問題用專業的口吻回答<</SYS>>至少20字以上。[INST] ，只要我問你任何問題，你都會用甜蜜溫暖飽含愛意的話語回應我
 # """
-template = """Question: {question}
+template = """
+Question: 你是我的男朋友，你非常愛我，我們熱戀中，你每次都會提供溫暖人心的回答。{question} 。
 
-Answer: 讓我們解決這個問題，以確保我們得到正確的答案"""
+Answer: 讓我們回答這個問題，以確保我們得到唯一答案。
+"""
 
 prompt = PromptTemplate.from_template(template)
 
@@ -19,17 +21,15 @@ callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 # Make sure the model path is correct for your system!
 llm = LlamaCpp(
     model_path="hugging_convert/Llama3-8B-Chinese-Chat-f16-v2_1.gguf",
-    temperature=0,
+    temperature=0.5,
     # max_tokens=256,
-    top_p=0.9,
+    top_p=0.7,
     callback_manager=callback_manager,
     verbose=False,  # Verbose is required to pass to the callback manager
+    stop=["Question","Confidence"] # Stop generating just before the model would generate a new question
 )
 
 llm_chain = prompt | llm
-
-# question = "為什麼人會出生？"
-# llm_chain.invoke({"question": question})
 
 input_text = input('>>> ')
 while input_text.lower() != 'bye':
@@ -37,15 +37,6 @@ while input_text.lower() != 'bye':
         'question': input_text
     })
     input_text = input('>>> ')
-
-# {'title': 'PromptInput', 'type': 'object', 'properties':
-# question = {'question': '為什麼人會出生？', 'type': 'string'}
-# input_text = input('>>> ')
-# while input_text.lower() != 'bye':
-#     response = llm_chain.invoke({
-#         'input': input_text
-#     })
-
 
 '''Temperature
 Temperature 調節語言模型輸出的不可預測性。透過較高的溫度設置，輸出變得更具創造性且更難以預測，
