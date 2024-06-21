c1ca88483d716444ed982d04e3a230d9bafdfdd7
weitsung50110 <a1233z51ab10@gmail.com>
2024-06-21 15:36:09 +0800

    modify llamaCpp_sys_lover.py comment out

--- langchain_llamaCpp_sys_lover.py
+++ langchain_llamaCpp_sys_lover.py
@@ -38,18 +38,4 @@ while input_text.lower() != 'bye':
     })
     input_text = input('>>> ')
 
-'''Temperature
-Temperature 調節語言模型輸出的不可預測性。透過較高的溫度設置，輸出變得更具創造性且更難以預測，
-因為它放大了不太可能的令牌的可能性，同時降低了更可能的令牌的可能性。
-相反，較低的溫度會產生更保守和可預測的結果。以下範例說明了輸出中的這些差異。
-
-Top P[^b] 是語言模型中的一項可設定的超參數，有助於管理其輸出的隨機性。它的工作原理是建立一個機率閾值，
-然後選擇組合可能性超過此限制的令牌。
-
-https://learnprompting.org/zh-tw/docs/basics/configuration_hyperparameters
-
-top-k 与 top-p 为选择 token 引入了随机性，让其他高分的 token 有被选择的机会，不像 greedy decoding 一样总是选最高分的。
-https://juejin.cn/post/7236558485290631205
-'''
-
 # https://python.langchain.com/v0.2/docs/integrations/llms/llamacpp/
